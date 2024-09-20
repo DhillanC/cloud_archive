d58d0bbb1efe0b6b1f4b5bb2cc0e4b82da2bdba1
DhillanC <dhillan.contreras@gmail.com>
2024-09-20 14:15:44 -0300

    Rearrange of folders.

--- docker_inca/test2/init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Wait for SQL Server to start
-sleep 30s
-
-# Run the initialization commands or scripts
-#/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Inca2024!" -i /usr/src/app/initialize-database.sql
-
-# Restore the database from the backup file
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Inca2024!" -Q "RESTORE DATABASE BDI_PRD FROM DISK = '/var/opt/mssql/backup/backupfile.bak' WITH REPLACE"
